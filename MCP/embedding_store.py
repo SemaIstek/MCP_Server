a6abbd6661ba1fd96449a8b56213f9a8b160cba3
Sema <sema.istek@powercoders.org>
2026-01-26 12:23:38 +0100

    Change the model for embedding

--- MCP/embedding_store.py
+++ MCP/embedding_store.py
@@ -2,7 +2,7 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 
 def create_faiss(documents):
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    embeddings = OllamaEmbeddings(model="all-MiniLM-L6-v2")
     vectorstore = FAISS.from_texts(
         texts=[doc.page_content for doc in documents],
         embedding=embeddings,
